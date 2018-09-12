57e2b12d2e093a15ba5b309b7abe3ad580dcafcd
Jose Escribano vega <msi_w@hotmail.com>
2018-09-12 12:10:16 +0200

    Dash del personaje V0.0.1

--- Assets/Scripts/Characters/Player/Player.cs
+++ Assets/Scripts/Characters/Player/Player.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class Player : Character
 {
-
+	private float dashDistance,dashCooldown, currentdashCooldown;
+	private Vector2 savedVelocity;
 
 	//CONSTRUCTOR
 	public Player(){
 	}
-	public Player (float life, float damage, float speed, Rigidbody2D rb)
+	public Player (float life, float damage, float speed,float dashDistance,float dashCooldown,Rigidbody2D rb)
 	{
 		this.life= life;
 		this.damage= damage;
 		this.speed= speed;
 		this.rb =rb;
+		this.dashDistance=dashDistance;
+		this.dashCooldown=dashCooldown;
+		this.currentdashCooldown= dashCooldown;
 	}
 
 
@@ -27,12 +31,58 @@ public class Player : Character
 
 	public void move(float moveHorizontal, float moveVertical)
 	{
+			//Movimiento
+			Vector2 movement_vector= new Vector2 (moveHorizontal, moveVertical);
+			movement_vector= movement_vector.normalized*speed;
 
-		Vector2 movement_vector= new Vector2 (moveHorizontal, moveVertical);
-		rb.MovePosition(rb.position+movement_vector*Time.deltaTime*speed);
+			rb.velocity= movement_vector*Time.deltaTime;
+			// rb.MovePosition(rb.position+movement_vector*Time.deltaTime);
+			// this.transform.Translate(movement_vector*Time.deltaTime);
 
-							
-		// Vector2 movement= new Vector2(moveHorizontal,moveVertical);
-		// this.transform.Translate(movement*speed*Time.deltaTime);
 	}
+
+	public void dash(float dashClick)
+	{
+		string dashState="";
+
+		//Dash
+		//Si se ha pulsado el dash
+		if(dashClick>0){ dashState= "ready";}
+
+		switch(dashState)
+		{
+			case "ready":
+				savedVelocity=rb.velocity;
+				rb.velocity= new Vector2 (rb.velocity.x*dashDistance, rb.velocity.y*dashDistance);
+				dashState="dashing";
+
+			break;
+
+			case "dashing":
+				currentdashCooldown+=Time.deltaTime*dashDistance;
+
+				if(currentdashCooldown>=dashCooldown)
+				{
+					currentdashCooldown=dashCooldown;
+					rb.velocity=savedVelocity;
+					dashState="Time";
+				}
+
+			break;
+
+			case "Time":
+				currentdashCooldown-=Time.deltaTime;
+				
+				if(currentdashCooldown<=0)
+				{
+					currentdashCooldown=0;
+					dashState="ready";
+				}
+
+			break;
+
+		}
+
+	}
+
 }
--- Assets/Scripts/Characters/Player/PlayerController.cs
+++ Assets/Scripts/Characters/Player/PlayerController.cs
@@ -5,23 +5,35 @@ using UnityEngine;
 //clase la cual ejecutara todo los métodos del player
 public class PlayerController : MonoBehaviour 
 {
-	public float life,speed,damage;
+	//VARIABLES
+	[Header("Caractrísticas del personaje")]
+	public float life;
+	public float damage;
+	[Header("Movimiento")]
+	public float speed;
+	public float dashDistance;
+	public float dashCooldown;
+	
 	private float moveHorizontal,moveVertical;
 	private Player player;
 	private Rigidbody2D rb;
-	
+	private float dashClick;
+
 	void Start () 
 	{
 		rb= GetComponent<Rigidbody2D>();
-		player= new Player(life, damage, speed,rb);
+		player= new Player(life, damage, speed,dashDistance,dashCooldown,rb);
+
 	}
 	
 	void Update () 
 	{
-
+		//------------------INPUTS------------------------
+		//movimiento
 		 moveHorizontal = Input.GetAxis("Horizontal");
 		 moveVertical = Input.GetAxis("Vertical");
-
+		//dash
+		dashClick=Input.GetAxis("Dash");//botón segundario del ratón
 	}
 
 	void FixedUpdate() 
@@ -30,6 +42,10 @@ public class PlayerController : MonoBehaviour
 		//		 Tanto del teclado como del yoystick
 		//----------------------------------------------------
 		player.move(moveHorizontal,moveVertical);
+		//---------------------DASH------------------------
+		//		 Salto del personaje a una dirección
+		//----------------------------------------------------
+		player.dash(dashClick);
 
  	}
 }
